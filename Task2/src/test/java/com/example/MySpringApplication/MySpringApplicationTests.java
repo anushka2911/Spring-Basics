eebcf8ec986dfe21bc3f318ee42fc8204c7de737
anushka <anushka.pandey@pratishthanventures.com>
2024-08-04 10:33:54 +0545

    Task2

---
+++ Task2/src/test/java/com/example/MySpringApplication/MySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
